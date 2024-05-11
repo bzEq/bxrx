2d73224c54fa562d38c106e4f07f0c22ff448be9
Kai Luo <gluokai@gmail.com>
2024-05-11 10:51:26 +0800

    Enhancement to http pack/unpack

--- core/protocol.go
+++ core/protocol.go
@@ -46,15 +46,21 @@ func (self *HTTPProtocol) Pack(b *IoVec, out *bufio.Writer) error {
 		return err
 	}
 	req.Header.Set("User-Agent", "Mozilla/5.0")
-	if req.GetBody == nil {
-		req.ContentLength = int64(b.Len())
-		req.GetBody = func() (io.ReadCloser, error) {
-			return io.NopCloser(b), nil
-		}
+	req.ContentLength = int64(b.Len())
+	if req.ContentLength <= 0 {
+		err = fmt.Errorf("Content length %d is abnormal", req.ContentLength)
+		log.Println(err)
+		return err
 	}
 	err = req.Write(out)
 	if err != nil {
 		log.Println(err)
+		return err
+	}
+	err = out.Flush()
+	if err != nil {
+		log.Println(err)
+		return err
 	}
 	return err
 }
@@ -73,8 +79,13 @@ func (self *HTTPProtocol) Unpack(in *bufio.Reader, b *IoVec) error {
 		log.Println(err)
 		return err
 	}
-	body := make([]byte, req.ContentLength)
-	if _, err = io.ReadFull(req.Body, body); err != nil {
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	if int64(len(body)) != req.ContentLength {
+		err = fmt.Errorf("Content length %d, %d bytes read in the body", req.ContentLength, len(body))
 		log.Println(err)
 		return err
 	}
